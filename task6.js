998406c1220f9b4c124b780451ef6345dd62fcda
priyagudla <114477054+Priya547@users.noreply.github.com>
2023-10-15 15:17:31 +0530

    Add files via upload

---
+++ task6.js
@@ -0,0 +1,44 @@
+
+function reading(){
+    console.log("priya is reading");
+}
+
+function writing(){
+    console.log("priya is writing");
+}
+
+function listening(){
+    console.log("priya is listening");
+}
+
+
+reading();
+
+
+function playing(){
+    console.log("children are playing");
+}
+
+playing();
+
+let digit1 = 1
+let digit2 = 4
+let digit3 = 3
+
+function addition(){
+    console.log(digit1+digit2);
+}
+
+addition();
+
+function subtraction(){
+    console.log(digit1-digit2);
+}
+
+subtraction();
+
+console.log("hey guys")
+
+for(let i=0; i<10; i++){
+    console.log("hey guys")
+}
